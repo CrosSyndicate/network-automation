2436a9b223074fed049904c7f6a09bf1425ded58
Casey Griffith <cagriff25@gmail.com>
2023-02-08 22:09:30 -0600

    get config to txt working

--- NETMIKO/IOSXR/netmiko_get_config_XR.py
+++ NETMIKO/IOSXR/netmiko_get_config_XR.py
@@ -1,22 +1,24 @@
-#script to connect to CISCO sandbox and create a file containing sandbox configuration data
+#Simple script to connect to CISCO XR sandbox and write config to a file named switch_running.txt
 
 from netmiko import ConnectHandler
 
-CSR = {
+XR = {
     "device_type":"cisco_ios",
-    "ip":"sandbox-iosxe-recomm-1.cisco.com",
-    "username":"developer",
+    "ip":"sandbox-iosxr-1.cisco.com",
+    "username":"admin",
     "password":"C1sco12345"
 }
 
-
-net_connect = ConnectHandler(**CSR)
+net_connect = ConnectHandler(**XR)
 print(net_connect.find_prompt())
 
 
-output = net_connect.send_command('sh ip int brief')
+output = net_connect.send_command('show running-config')
+save_file = open("switch_running.txt","w")
+save_file.write(output)
+save_file.close()
+
+#this prints what was written into the txt file
 print(output)
 
 net_connect.disconnect()
-
-#print(CSR)
