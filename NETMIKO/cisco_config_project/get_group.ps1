476b01774c042b88336250043100f0969535e572
Casey Griffith <cagriff25@gmail.com>
2023-02-16 21:28:35 -0600

    setting up CSV and get group ps1

--- NETMIKO/cisco_config_project/get_group.ps1
+++ NETMIKO/cisco_config_project/get_group.ps1
@@ -1,21 +1,10 @@
 $adgroups = "group1", "group2"
-# changed group names to generic names for at home work
-
-#$filter = (EmailAddress -notlike '*@DOMAIN.com') - passing this variable into function broke it, no output
-#The above comment was solved using a custom object with @{name = "groupname";expression ={$group}}
-
-
 $results = @();
 
 foreach ($group in $adgroups) 
-
 {
-   $results+= (Get-ADGroupMember -Identity $group -Recursive | Get-ADUser -Properties DisplayName | Select-Object name, SamAccountName, DisplayName, @{name = "groupname";expression ={$group}})
+   $results+= (Get-ADGroupMember -Identity $group -Recursive | Get-ADUser -Properties DisplayName | Select-Object DisplayName, @{name = "groupname";expression ={$group}})
 
 }
 
-$results | Export-CSV -Path “group_output.csv” -NoTypeInformation -Force
-
-#added to the $results CSV export, -NoTypeInformation to remove header in CSV - just making it cleaner to read
-
-# -Filter {(EmailAddress -notlike '*@DOMAIN.com')} - Trying to use this to filter only non @DOMAIN.com emails - it's pulling every non DOMAIN email
+$results | Export-CSV -Path “group_users.csv” -NoTypeInformation -Force
