a1816778e20779a6db28b405139e3007ea09219b
Casey Griffith <cagriff25@gmail.com>
2023-02-21 06:22:40 -0600

    clarifying comments

--- NETMIKO/IOSXR/ConnectHandler_cisco_XR.py
+++ NETMIKO/IOSXR/ConnectHandler_cisco_XR.py
@@ -2,6 +2,7 @@
 
 from netmiko import ConnectHandler
 
+#define the device
 XR = {
     "device_type":"cisco_ios",
     "ip":"sandbox-iosxr-1.cisco.com",
@@ -13,6 +14,7 @@ XR = {
 #net_connect = ConnectHandler(device_type="cisco_ios",host="sandbox-iosxe-recomm-1.cisco.com",username="developer", password="C1sco12345")
 
 net_connect = ConnectHandler(**XR)
+#prints the current network device prompt
 print(net_connect.find_prompt())
 
 
