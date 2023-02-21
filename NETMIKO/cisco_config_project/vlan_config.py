0c0ef44e7dabae7f58c82d74f53a3a413707e0cf
Casey Griffith <cagriff25@gmail.com>
2023-02-21 05:01:55 -0600

    csv iterate practice

---
+++ NETMIKO/cisco_config_project/vlan_config.py
@@ -0,0 +1,18 @@
+# Goals of this script:
+# 1. Import and read contents of group_users.csv
+# 2. Connect to cisco switch and configure a VLAN based on user data in group_users.csv
+
+
+
+
+
+
+
+
+
+
+
+
+
+###GRAVEYARD and Testing###
+
